896c942f62277a3e0d02d6718f33eb8b0163bcf7
Poukisans <93792388+Poukisans@users.noreply.github.com>
2024-12-16 02:18:27 +0100

    bons plans template

---
+++ controllers/BonsPlansController.php
@@ -0,0 +1,31 @@
+<?php
+require_once('./views/View.php');
+require_once('./controllers/Controller.php');
+
+class BonsPlansController extends Controller
+{
+    private $_view;
+    private $layoutContent;
+
+    public function __construct($url)
+    {
+        parent::__construct($url[0]);
+
+        if ($url[0] === "bons-plans") {
+                $this->page($url[0]);
+        } else {
+            throw new Exception('Page not found');
+        }
+    }
+
+    private function page($url)
+    {
+        // ====== Contenu général du layout ======
+        $this->layoutContent = $this->getLayoutContent($url);
+
+        $this->_view = new View("views/" . $url . ".php", [
+            'layoutContent' => $this->layoutContent,
+            'no_index' => 1,
+        ]);
+    }
+}
