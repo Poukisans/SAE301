e84e280ced87d741ec1d4825f2ff6208b8583b88
Poukisans <al.andr@hotmail.fr>
2025-01-20 00:16:23 +0100

    article front

--- models/ArticleModel.php
+++ models/ArticleModel.php
@@ -3,92 +3,202 @@ require_once 'Model.php';
 
 class ArticleModel extends Model
 {
-  // Liste 
-  public function getList()
-  {
-
-    $sql = "SELECT 
-    a.id,
-    a.nom,
-    a.lien,
-    a.miniature,
-    a.categorie,
-    a.prix,
-    CASE 
-        WHEN p.id IS NOT NULL AND NOW() BETWEEN p.date_debut AND p.date_fin THEN 
-            CASE 
-                WHEN p.type = 0 THEN a.prix - (a.prix * p.promotion / 100)
-                WHEN p.type = 1 THEN p.promotion
-                WHEN p.type = 2 THEN 'lot'
-                ELSE a.prix
-            END
-        ELSE null
-    END AS 'promotion'
-FROM 
-    b_articles a
-LEFT JOIN 
-    b_promotion_articles pa ON a.id = pa.id_article
-LEFT JOIN 
-    b_promotions p ON pa.id_promotion = p.id
-ORDER BY 
-    a.nom;";
-
-    $statment = $this->executerRequete($sql);
-    return $statment->fetchAll(PDO::FETCH_ASSOC);
-  }
-
-  public function getListCategory($categorie)
-  {
-
-    $sql = "SELECT 
-    a.id,
-    a.nom,
-    a.lien,
-    a.miniature,
-    a.categorie,
-    CASE 
-        WHEN p.id IS NOT NULL AND NOW() BETWEEN p.date_debut AND p.date_fin THEN p.rabais
-        ELSE NULL
-    END AS rabais,
-    CASE 
-        WHEN p.id IS NOT NULL AND NOW() BETWEEN p.date_debut AND p.date_fin THEN p.prix_force
-        ELSE NULL
-    END AS prix_force,
-    CASE 
-        WHEN p.id IS NOT NULL AND NOW() BETWEEN p.date_debut AND p.date_fin THEN 
-            CASE 
-                WHEN p.prix_force IS NOT NULL THEN p.prix_force
-                WHEN p.rabais IS NOT NULL THEN a.prix - (a.prix * p.rabais / 100)
-                ELSE a.prix
-            END
-        ELSE a.prix
-    END AS prix
-FROM 
-    b_articles a
-LEFT JOIN 
-    b_promotion_articles pa ON a.id = pa.id_article
-LEFT JOIN 
-    b_promotions p ON pa.id_promotion = p.id
-WHERE
-    a.categorie = :categorie
-ORDER BY 
-    a.nom;
-   ";
-
-    $statment = $this->executerRequete($sql, [':categorie' => $categorie]);
-    return $statment->fetchAll(PDO::FETCH_ASSOC);
-  }
-
-  public function getInfo($action)
-  {
-    $sql = "SELECT * FROM b_articles WHERE lien = :action;";
-    $statment = $this->executerRequete($sql, [':action' => $action]);
-    $content = $statment->fetch(PDO::FETCH_ASSOC);
-
-    if (!$content) {
-      throw new Exception("404 Not Found : Article inconnu.");
+    // ==================================== LISTE ====================================
+    public function getList()
+    {
+        $sql = "SELECT 
+                a.id, 
+                a.nom, 
+                a.prix, 
+                a.miniature, 
+                a.affichage, 
+                a.affichage_accueil, 
+                a.categorie, 
+                a.lien, 
+                p.type AS type_promotion, 
+                p.promotion AS taux_promotion,
+                CASE 
+                    WHEN p.id IS NOT NULL AND CURDATE() BETWEEN p.date_debut AND p.date_fin THEN 
+                        CASE 
+                            WHEN p.type = 0 THEN a.prix - (a.prix * p.promotion / 100)
+                            WHEN p.type = 1 THEN p.promotion
+                            WHEN p.type = 2 THEN 'lot'
+                            ELSE a.prix
+                        END
+                    ELSE NULL
+                END AS promotion,
+                COUNT(com.id) AS commentaire, -- Nombre total de commentaires pour l'article
+                ROUND(AVG(com.note), 2) AS note -- Moyenne des notes (arrondie à 2 décimales)
+            FROM 
+                b_articles a
+            LEFT JOIN 
+                b_promotion_articles pa ON a.id = pa.id_article
+            LEFT JOIN 
+                b_promotions p ON pa.id_promotion = p.id
+            LEFT JOIN 
+                b_article_commentaires com ON com.id_article = a.id
+            WHERE
+                a.affichage = 1
+            GROUP BY 
+                a.id, a.nom, a.prix, a.miniature, a.affichage, a.affichage_accueil, a.categorie, a.lien, 
+                p.type, p.promotion, p.id, p.date_debut, p.date_fin
+            ORDER BY 
+                a.nom;";
+        $statment = $this->executerRequete($sql);
+        return $statment->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    return $content;
-  }
+    // ==================================== INFO ====================================
+    public function getInfo($content)
+    {
+        $sql = "SELECT 
+                a.*,
+                p.type,
+                p.id AS id_promotion,
+                CASE 
+                    WHEN p.id IS NOT NULL AND CURDATE() BETWEEN p.date_debut AND p.date_fin THEN 
+                        CASE 
+                            WHEN p.type = 0 THEN a.prix - (a.prix * p.promotion / 100)
+                            WHEN p.type = 1 THEN p.promotion
+                            WHEN p.type = 2 THEN 'lot'
+                            ELSE a.prix
+                        END
+                    ELSE null
+                END AS 'promotion'
+            FROM 
+                b_articles a
+            LEFT JOIN 
+                b_promotion_articles pa ON a.id = pa.id_article
+            LEFT JOIN 
+                b_promotions p ON pa.id_promotion = p.id
+            WHERE
+                a.lien = :lien
+            ORDER BY 
+                a.nom;";
+        $statment = $this->executerRequete($sql, [':lien' => $content]);
+
+        $content = $statment->fetch(PDO::FETCH_ASSOC);
+
+        if (!$content) {
+            throw new Exception("404 Not Found : Article inconnu.");
+        }
+
+        return $content;
+    }
+
+    // ==================================== AJOUT ====================================
+    public function add($data)
+    {
+        extract($data);
+
+        try {
+            $sql = "INSERT INTO b_articles (nom, categorie, lien) VALUES (:nom, :categorie, :lien)";
+            $this->executerRequete($sql, [
+                ':nom' => $nom,
+                ':categorie' => $categorie,
+                ':lien' => $lien,
+            ]);
+        } catch (PDOException $e) {
+            throw new Exception("Erreur lors de la mise à jour en BDD: " . $e->getMessage());
+        }
+    }
+
+    // ==================================== UPDATE INFO ====================================
+    public function update($data)
+    {
+        extract($data);
+
+        try {
+            $sql = "UPDATE b_articles SET nom = :nom, prix = :prix, categorie = :categorie, description = :description, lien = :lien  WHERE id = :id";
+            $this->executerRequete($sql, [
+                ':id' => $id,
+                ':nom' => $nom,
+                ':prix' => $prix,
+                ':categorie' => $categorie,
+                ':description' => $description,
+                ':lien' => $lien,
+            ]);
+        } catch (PDOException $e) {
+            throw new Exception("Erreur lors de la mise à jour en BDD: " . $e->getMessage());
+        }
+    }
+
+    // ==================================== UPDATE MINIATURE ====================================
+    public function updateMiniature($id, $miniaturePath)
+    {
+        try {
+            $sql = "UPDATE b_articles SET miniature = :miniature WHERE id = :id";
+            $this->executerRequete($sql, [
+                ':miniature' => $miniaturePath,
+                ':id' => $id
+            ]);
+        } catch (PDOException $e) {
+            throw new Exception("Erreur lors de la mise à jour en BDD: " . $e->getMessage());
+        }
+    }
+
+    // ==================================== SUPPRESSION ====================================
+    public function delete($data)
+    {
+        try {
+            $sql = "DELETE FROM b_articles WHERE id = :id ";
+            $this->executerRequete($sql, [
+                ':id' => $data,
+            ]);
+        } catch (PDOException $e) {
+            throw new Exception("Erreur lors de la mise à jour en BDD: " . $e->getMessage());
+        }
+    }
+
+    // ==================================== UPDATE INFO COMPOSITION ====================================
+    public function updateInfo($data)
+    {
+        extract($data);
+
+        try {
+            $sql = "UPDATE b_articles SET composition = :composition, taille = :taille, entretien = :entretien, fabrication = :fabrication WHERE id = :id";
+            $this->executerRequete($sql, [
+                ':id' => $id,
+                ':composition' => $composition,
+                ':taille' => $taille,
+                ':entretien' => $entretien,
+                ':fabrication' => $fabrication,
+            ]);
+        } catch (PDOException $e) {
+            throw new Exception("Erreur lors de la mise à jour en BDD: " . $e->getMessage());
+        }
+    }
+
+    // ==================================== AFFICHER/MASQUER ====================================
+    public function setDisplay($data)
+    {
+        extract($data);
+
+        try {
+            $sql = "UPDATE b_articles SET affichage = :affichage WHERE id = :id";
+            $this->executerRequete($sql, [
+                ':id' => $id,
+                ':affichage' => $affichage,
+            ]);
+        } catch (PDOException $e) {
+            throw new Exception("Erreur lors de la mise à jour en BDD: " . $e->getMessage());
+        }
+    }
+
+    // ==================================== AJOUT ====================================
+    public function setAffichageAccueil($affichage_accueil, $selected_articles)
+    {
+        try {
+            $sql = "UPDATE b_articles SET affichage_accueil = :affichage_accueil WHERE id = :id";
+
+            foreach ($selected_articles as $id) {
+                $this->executerRequete($sql, [
+                    ':id' => $id,
+                    ':affichage_accueil' => $affichage_accueil,
+                ]);
+            }
+        } catch (PDOException $e) {
+            throw new Exception("Erreur lors de la mise à jour en BDD : " . $e->getMessage());
+        }
+    }
 }
--- views/tee-shirts.php
+++ views/tee-shirts.php
@@ -1,23 +1,78 @@
-<?php
-foreach ($articleList as $content) {
-?>
-    <div>
-        <img src=".<?= $content['miniature'] ?>" style="width: 30px;">
-        <a href="tee-shirts/<?= $content['categorie'] ?>/<?= $content['lien'] ?>"><?= $content['nom'] ?></a>
-        <p><?= number_format($content['prix'],2) ?> €</p>
-        <?php
-        if (!empty($content['rabais'])) {
-        ?>
-            <p>-<?= $content['rabais'] ?>%</p>
-        <?php
-        } elseif (!empty($content['prix_force'])) {
-        ?>
-            <p><?= $content['prix_force'] ?></p>
-        <?php
-        }
-        ?>
+<div class="bread_crumb">
+    <a class="grey" href="#">Accueil</a> > <a class="grey" href=".tee-shirts">Tee-shirts</a>
+</div>
 
+<div class="section_header container">
+    <div class="row title_container">
+        <div class="banner_sup">
+            <h3 class="inter_title banner_text">Nos tee-shirts</h3>
+            <img class="img_banner" src="<?= $layoutContent['banner'] ?>" alt="">
+        </div>
     </div>
-<?php
-}
-?>
+</div>
+
+<div class="mini_bar">
+    <p class="nombre_article_list"><?=count($articleList)?> articles</p>
+    <div class="custom_filtre">
+        <select name="filtres" id="filtres">
+            <option value="prix_croissant">Prix croissant </option>
+            <option value="prix_decroissant">Prix décroissant </option>
+        </select>
+    </div>
+</div>
+
+<div class="article_list">
+    <?php
+    foreach ($articleList as $content) {
+    ?>
+        <div class="article">
+            <a class="nom_article" href="tee-shirts/<?= $content['categorie'] ?>/<?= $content['lien'] ?>">
+                <img class="miniature" src="<?= $content['miniature'] ?>"></a>
+            <div class="article_text">
+                <div class="nom_prix">
+                    <a class="nom_article" href="tee-shirts/<?= $content['categorie'] ?>/<?= $content['lien'] ?>"><?= $content['nom'] ?></a>
+                    <div class="container_prix">
+                        <?php
+                        if (!empty($content['promotion'])) {
+
+                            if ($content['type_promotion'] == 0) {
+                        ?>
+                                <p class="prix_prom"><?= number_format($content['promotion'], 2) ?>€</p>
+                                <p class="prix_barre"><?= $content['prix'] ?>€</p>
+                                <p class="tag_solde">-<?= $content['taux_promotion'] ?>%</p>
+
+                            <?php
+                            } elseif ($content['type_promotion'] == 1) {
+                            ?>
+                                <p class="prix_prom"><?= $content['promotion'] ?></p>
+                                <p class="prix_barre"><?= $content['prix'] ?></p>
+
+                            <?php
+                            }
+                        } else {
+                            ?>
+                            <p class="prix"><?= $content['prix'] ?></p>
+                        <?php
+                        }
+                        ?>
+                    </div>
+                </div>
+                <?php
+                if (!empty($content['commentaire'])) {
+                ?>
+                    <div class="note_avis">
+                        <div class="container_note">
+                            <div class="star"></div>
+                            <p class="note"><?= $content['note'] ?></p>
+                        </div>
+                        <p class="avis"><?= $content['commentaire'] ?></p>
+                    </div>
+                <?php
+                }
+                ?>
+            </div>
+        </div>
+    <?php
+    }
+    ?>
+</div>
