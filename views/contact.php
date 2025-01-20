2e3f937888fa9bb59a1956d012009e9d5774e9ed
Poukisans <al.andr@hotmail.fr>
2025-01-20 05:04:03 +0100

    link nom section

--- views/contact.php
+++ views/contact.php
@@ -2,17 +2,17 @@
     <div class="section_header container">
         <div class="row title_container">
             <div class="banner_sup">
-                <h3 class="inter_title banner_text">Contact</h3>
-                <img class="img_banner" src="Image_Pub_4.jpg" alt="">
+                <h3 class="inter_title banner_text"><?= $layoutContent['sectionNom'] ?></h3>
+                <img class="img_banner" src="<?= $layoutContent['banner'] ?>" alt="">
             </div>
         </div>
     </div>
 
     <div class="text mb">
         <div class="text_part mb">
-            <h2 class="inter_title uppercase mb"><?=$contactInfo['contact_title']?></h2>
-            <?=$contactInfo['contact']?>
+            <h2 class="inter_title uppercase mb"><?= $contactInfo['contact_title'] ?></h2>
+            <?= $contactInfo['contact'] ?>
         </div>
-        <a class="button_red inter_secondary mb" href="<?= $layoutContent['sectionList'][9]['lien'] ?>">FAQ</a>
+        <a class="button_red inter_secondary mb" href="./<?= $layoutContent['sectionList'][9]['lien'] ?>">FAQ</a>
     </div>
 </section>
